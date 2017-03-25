67a20e90d5b38464c4955edbd82f8036a2f40a36
volving <volving@icloud.com>
2017-03-26 00:32:38 +0800

    Add: gitignore~~ Add: Mocha + expect Add: basic test for returnArrayOfNumbers.js

--- returnArrayOfNumbers.js
+++ returnArrayOfNumbers.js
@@ -1,30 +1,93 @@
-// Question: How to get n numbers inbetween [2, 32]?
+// Question: 取[2, 32]之间的数字数组
+var expect = require('expect');
 
 const LIMIT = 31;
 
-function fn(n){
-	if(typeof n === 'number' && !isNaN(n)){
-		if(n > LIMIT){
-			return new Error(`No more than ${LIMIT}!`);
-		}
-		var validAmount = 0;
-		var obj = {};
-		var item = 0;
-		var result = [];
-		for(;validAmount < n;){
-			item = 2 + parseInt(Math.random() * 31);
-			obj[item] = 1;
-			result = Object.keys(obj)||[];
-			validAmount = result.length;
-		}
-		return result;
-	}
-	return new Error(`Input parameter should be a valid number, but "${n}" entered!`);
+function fn(n) {
+    debugger;
+    if (typeof n === 'number' && !isNaN(n)) {
+        n = parseInt(n);
+        if (n > LIMIT) {
+            console.log(n);
+            // throw new Error(`No more than ${LIMIT}!`);
+            return `No more than ${LIMIT}!`;
+        }
+        if (n < 1) {
+            return `Non-positive numbers are forbidden!`;
+            // throw new Error(`Negative numbers are forbidden!`);
+        }
+        var validAmount = 0;
+        var obj = {};
+        var item = 0;
+        var result = [];
+        for (; validAmount < n;) {
+            item = 2 + parseInt(Math.random() * 31);
+            obj[item] = 1;
+            result = Object.keys(obj) || [];
+            validAmount = result.length;
+        }
+        return result;
+    }
+    // throw new Error(`Input parameter should be a valid number, but "${n}" entered!`);
+    return `Input parameter should be a valid number, but "${n}" entered!`;
 }
-
+/*
 console.log(fn(1));
 console.log(fn(3));
 console.log(fn(31));
 console.log(fn(33));
 console.log(fn('asd'));
 console.log(fn());
+*/
+var voodoo = 3;
+
+// expect(3).toEqual(3);
+// expect(fn.bind(voodoo)).toThrow(/Input parameter should be a valid number, but/);
+
+// expect(function(){
+//     throw new Error('boom!');
+// }).toThrow(/boom/);
+function runNP(n) {
+    var array = fn(n);
+    return array;
+}
+
+function runP(n) {
+    var array = fn(n);
+    return array.length;
+}
+
+describe('numbers', () => {
+    describe('Non-positive numbers', () => {
+        it('Should throw a Error with "Non-positive numbers are forbidden!" when fn(-1)', () => {
+            expect(runNP(-1)).toEqual('Non-positive numbers are forbidden!');
+        });
+        it('Should throw a Error with "Non-positive numbers are forbidden!" when fn(-parseInt(Math.random()*1000))', () => {
+            expect(runNP((-parseInt(Math.random() * 1000)))).toEqual('Non-positive numbers are forbidden!');
+        });
+        it('Should throw a Error with "Non-positive numbers are forbidden!" when fn(0)', () => {
+            expect(runNP(0)).toEqual('Non-positive numbers are forbidden!');
+        });
+        it('Should throw a Error with "Non-positive numbers are forbidden!" when fn(0.1)', () => {
+            expect(runNP(0.1)).toEqual('Non-positive numbers are forbidden!');
+        });
+    });
+    describe('Positive numbers [1, 30]', () => {
+        it('Should be 1 when fn(1)', () => {
+            expect(runP(1)).toEqual(1);
+        });
+        it('Should be 3 when fn(3)', () => {
+            expect(runP(3)).toEqual(3);
+        });
+        var val1 = parseInt(Math.random()*31+1);
+        it(`Should be ${val1} when fn(${val1})`, () => {
+            expect(runP(val1)).toEqual(val1);
+        });
+    });
+    describe('Positive numbers over 30', () => {
+        var val2 = Math.random()*1024 + 31;
+        it(`Should be No more than ${LIMIT}! when fn(${val2})`, () => {
+            expect(runNP(val2)).toEqual(`No more than ${LIMIT}!`);
+        });
+    });
+});
