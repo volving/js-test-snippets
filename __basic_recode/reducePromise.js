3d46bfae4a95608b6235b1540e04a768ce62ee53
volving <volving@icloud.com>
2021-02-04 22:00:37 +0800

    Mdf: reducePromise: fixed test case

--- __basic_recode/reducePromise.js
+++ __basic_recode/reducePromise.js
@@ -2,38 +2,43 @@
  *
  * @param  {Array of promise functions} taskList: promise functions
  */
-function runPromiseQue (taskList, args) {
+function reducePromise(taskList, args) {
     console.log('taskList: ', taskList.length);
 
     let pre = taskList.shift();
-    taskList.reduce(function (pre, cur) {
-        return pre.then(ret => {
-            return cur(ret);
-        });
-    }, Promise.resolve(pre.call(args, 99)).catch(e => {
-        console.log(e);
-    }))
+    taskList.reduce(
+        function (pre, cur) {
+            return pre.then((ret) => {
+                return cur(ret);
+            });
+        },
+        Promise.resolve(pre.call(null, args)).catch((e) => {
+            console.log(e);
+        })
+    );
 }
 
+/*
 
-let fnOne = fnTwo = fnThree = function (val) {
+let fnOne = fnTwo = fnThree = fnFour = function (val) {
     // console.log('this: ', this);
-    console.log('>>>', new Date());
+    console.log('>>>', val, new Date());
     return new Promise(function (resolve, reject) {
         // console.log(`fnOne: , ${val}, ${typeof val}`);
-
-        let tmp = Math.random() * 10000;
+        let tmp = Math.random() * 1000;
         setTimeout(function () {
             console.log('---', new Date());
-            typeof val === 'number' ? resolve(parseInt(tmp)) : resolve(NaN);
+            typeof val !== 'number' ? resolve(parseInt(tmp)) : resolve(val + 1);
         }, tmp)
     })
 }
 
 debugger;
 
-let taskList = [fnOne, fnTwo, fnThree];
+let taskList = [fnOne, fnTwo, fnThree, fnFour];
 
-// console.log('taskList: ', Object.prototype.toString.call(taskList));
+reducePromise(taskList, {});
 
-runPromiseQue(taskList, NaN);
+*/
+
+module.exports = reducePromise;
