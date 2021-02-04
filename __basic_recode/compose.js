64d90a042bd2b8ad77288de145863626f1d4d7ad
volving <volving@icloud.com>
2021-02-04 21:37:55 +0800

    Mdf: compose functions and return the trigger function

--- __basic_recode/compose.js
+++ __basic_recode/compose.js
@@ -1,9 +1,41 @@
+const { getType } = require('./utils');
 /**
  *
  * @param  {...any} args array of functions
  */
-function compose (...args) {
-
+// fn: wrap returns in []
+function compose (fnList, ...args) {
+    fnList.reverse();
+    let ret;
+    return function () {
+        return fnList.reduce(function (pre, fn) {
+            return fn.call(null, pre);
+        }, args)
+    }
 }
+
+/**
+ * Test cas
+ */
+/*
+function a (val) {
+    console.log('a');
+    return `a(${val})`;
+}
+
+function b (val) {
+    console.log('b');
+    return `b(${val})`;
+}
+
+function c(val) {
+    console.log('c');
+    return `c(${val})`;
+}
+
+
+let fuse = compose([ a, b, c ], 'lollipop');
+console.log(fuse())
+*/
 
 module.exports = compose;
