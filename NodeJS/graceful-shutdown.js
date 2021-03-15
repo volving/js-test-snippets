be6a6a35ff68826c7f1ce29010935e7e7d20f048
xvno <45987576+xvno@users.noreply.github.com>
2021-03-15 21:28:19 +0800

    Add: node.js: gracefully-shutdown a node process

---
+++ NodeJS/graceful-shutdown.js
@@ -0,0 +1,9 @@
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => {})
+        .then(() => process.exit());
+};
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
